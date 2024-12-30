9cbb74d614c5000e55eda0ea90074647e7dea627
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2024-12-30 18:01:41 -0300

    Refactor and clean up

--- script.js
+++ script.js
@@ -3,33 +3,30 @@ const SUBTRACT = "-";
 const MULTIPLY = "x";
 const DIVIDE = "/";
 const NO_OPERATION = "no_operation";
+const ERROR_DIVISION = "I guess we'll never know...";
+
 const FIRST_OPERAND = 1;
 const SECOND_OPERAND = 2;
-
 let currentOperand = FIRST_OPERAND;
+
 let firstOperand = 0;
 let secondOperand = 0;
 let operator = NO_OPERATION;
 let result = null;
 
-// Get primary display element
-const primaryDisplay = document.querySelector(".primary-display");
-primaryDisplay.textContent = "0";
+let isOperatorActive = false;
 
-// Get secondary display element
+const primaryDisplay = document.querySelector(".primary-display");
 const secondaryDisplay = document.querySelector(".secondary-display");
+
+primaryDisplay.textContent = "0";
 secondaryDisplay.textContent = "Enter the first number";
 
 // Handle operand button click
-// - At first use, the firstOperand is updated.
-// - After an operator is clicked, the currentOperand is SECOND_OPERAND
-//   and the secondOperand is updated.
-// - If an operator is clicked, and instead of operate the user clicks
-//   an operator again, then the result becomes the first operand,
-//   and the secondOperand is updated.
 const operandButtons = document.querySelectorAll(".operand-btn");
 operandButtons.forEach(btn => btn.addEventListener("click", () => {
 
+    // Clear all if there is a result and a new number is being typed
     if (operator == NO_OPERATION && result != null) {
         clearAll();
     }
@@ -41,50 +38,47 @@ operandButtons.forEach(btn => btn.addEventListener("click", () => {
 
     primaryDisplay.textContent += btn.textContent;
 
-    currentOperand == FIRST_OPERAND ? 
-        firstOperand  = Number(primaryDisplay.textContent):
-        secondOperand = Number(primaryDisplay.textContent);
+    updateOperand();
 }));
 
 // Handle operator buttons click
-// - If an operator is clicked, update the secondaryDisplay with the primaryDisplay content
-// - If another operator is clicked without clicking operate, then automatically
-//   operate
 const operatorButtons = document.querySelectorAll(".operator-btn");
 operatorButtons.forEach(btn => btn.addEventListener("click", () => {
-    const lastOperatorButton = document.querySelector(".active-operator");
-    if(lastOperatorButton != null) {
-    
-        operate();
-        lastOperatorButton.classList.remove("active-operator");
+    if (primaryDisplay.textContent == ERROR_DIVISION) {
+        clearAll();
+        return;
     }
 
+    if (isOperatorActive) {
+        document.querySelector(".active-operator").classList.remove("active-operator");
+        operate();
+    }
+    
+    operator = btn.textContent;
+    isOperatorActive = true;
+    currentOperand = SECOND_OPERAND;
+    
     btn.classList.add("active-operator");
 
-    currentOperand = SECOND_OPERAND;
-    operator = btn.textContent;
-    secondaryDisplay.textContent = primaryDisplay.textContent + " " + btn.textContent;
+    secondaryDisplay.textContent = primaryDisplay.textContent + " " + operator;
     primaryDisplay.textContent = "0";
+    secondOperand = 0;
 }));
 
 // Handle operate (=) button click
 const operateButton = document.querySelector(".operate-btn")
 operateButton.addEventListener("click", () => {
-    secondOperand = Number(primaryDisplay.textContent);
-
-    // Removes any character that is not a digit
-    if (!Number.isFinite(secondaryDisplay.textContent)) {
-        secondaryDisplay.textContent = primaryDisplay.textContent;
-    } else {
-        secondaryDisplay.textContent += " " + primaryDisplay.textContent;
+    if (isOperatorActive) {
+        document.querySelector(".active-operator").classList.remove("active-operator");
+        isOperatorActive = false;
     }
-
+    
+    updateOperand();
     operate();
 });
 
 // Handle Clear all (AC) button click
-const clearAllButton = document.querySelector(".clear-all-btn");
-clearAllButton.addEventListener("click", clearAll);
+document.querySelector(".clear-all-btn").addEventListener("click", clearAll);
 
 // Handle Clear (C) button click
 const clearButton = document.querySelector(".clear-btn");
@@ -92,19 +86,17 @@ const clearButton = document.querySelector(".clear-btn");
 // Handle Signed (+/-) button click
 const signedButton = document.querySelector(".signed-btn");
 
-function clearAll() {
-    firstOperand = 0;
-    secondOperand = 0;
-    operator = NO_OPERATION;
-    result = null;
-    currentOperand = FIRST_OPERAND;
-    primaryDisplay.textContent = "0";
-    secondaryDisplay.textContent = "Enter the first number";
+// Automatically updates operand according to the currentOperand value
+function updateOperand() {
+    currentOperand == FIRST_OPERAND ? 
+        firstOperand  = Number(primaryDisplay.textContent):
+        secondOperand = Number(primaryDisplay.textContent);
 }
 
 // Operates according to the operator value, usign firstOperand and secondOperand.
 // Throws console.error() if there are invalid values
 function operate() {
+
     switch (operator) {
         case SUM:
             result = firstOperand + secondOperand;
@@ -116,6 +108,17 @@ function operate() {
             result = firstOperand * secondOperand;
             break;
         case DIVIDE:
+            if (secondOperand == 0 || !Number.isFinite(firstOperand / secondOperand)) {
+                primaryDisplay.textContent = ERROR_DIVISION;
+                secondaryDisplay.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
+                firstOperand = 0;
+                secondOperand = 0;
+                currentOperand = FIRST_OPERAND;
+                operator = NO_OPERATION;
+                result = null;
+                return;
+            }
+
             result = firstOperand / secondOperand;
             break;
         case NO_OPERATION:
@@ -123,10 +126,10 @@ function operate() {
             result = firstOperand;
             break;
         default:
-            if (!Number.isInteger(firstOperand))
-                console.error(`First number ${firstOperand} is not integer`);
-            else if (!Number.isInteger(secondOperand))
-                console.error(`Second number ${secondOperand} is not integer`);
+            if (!Number.isFinite(firstOperand))
+                console.error(`First operand ${firstOperand} is not valid`);
+            else if (!Number.isFinite(secondOperand))
+                console.error(`Second operand ${secondOperand} is not valid`);
             else if (operator !== "+" || operator !== "-" || operator !== "x" || operator !== "/")
                 console.error(`Invalid operator "${operator}"`);
             else
@@ -134,14 +137,24 @@ function operate() {
             break;
     }
 
-    if(operator != NO_OPERATION) {
-        secondaryDisplay.textContent = `${firstOperand} ${operator} ${secondOperand}`;
-        document.querySelector(".active-operator").classList.remove("active-operator");
+    if (operator != NO_OPERATION) {
+        secondaryDisplay.textContent = `${firstOperand} ${operator} ${secondOperand} =`;
     }
     
     primaryDisplay.textContent = result;
     firstOperand = result;
     secondOperand = 0;
-    operator = NO_OPERATION;
     currentOperand = FIRST_OPERAND;
+    operator = NO_OPERATION;
+}
+
+function clearAll() {
+    firstOperand = 0;
+    secondOperand = 0;
+    operator = NO_OPERATION;
+    isOperatorActive = false;
+    result = null;
+    currentOperand = FIRST_OPERAND;
+    primaryDisplay.textContent = "0";
+    secondaryDisplay.textContent = "Enter the first number";
 }
